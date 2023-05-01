d12bf32a1b64c0279cad0fb7e3fb8bde18e814e8
Jabor <lja@posteo.de>
2023-05-01 13:33:01 +0200

    Revert "Attempt to fix dialogs in combat"
    This reverts commit 3ddf32c5edb319902ce6a0295e2e79d7078313d8.

--- Include/GWCA/Managers/AgentMgr.h
+++ Include/GWCA/Managers/AgentMgr.h
@@ -34,7 +34,7 @@ namespace GW {
     namespace Agents {
         // === Dialogs ===
         // Same as pressing button (id) while talking to an NPC.
-        GWCA_API void SendDialog(uint32_t dialog_id); 
+        GWCA_API bool SendDialog(uint32_t dialog_id);
 
         // Returns last dialog id sent to the server. Requires the hook.
         GWCA_API bool GetIsAgentTargettable(const GW::Agent* agent);
