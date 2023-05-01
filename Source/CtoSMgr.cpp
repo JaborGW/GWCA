7db3c9576bb1ad479250f122c400fb47a56404b9
Jabor <lja@posteo.de>
2023-05-01 16:54:10 +0200

    Enable CtoS, opening chests at range

--- Source/CtoSMgr.cpp
+++ Source/CtoSMgr.cpp
@@ -38,9 +38,7 @@ namespace {
     uintptr_t game_srv_object_addr;
 
     void Init() {
-#ifndef GWCA_CTOS_DISABLED
         SendPacket_Func = (SendPacket_pt)Scanner::FindAssertion("p:\\code\\net\\msg\\msgconn.cpp", "bytes >= sizeof(dword)", -0x67);
-#endif
         uintptr_t address = Scanner::FindAssertion("p:\\code\\gw\\net\\cli\\gcgamecmd.cpp","No valid case for switch variable 'code'", -0x32);
         if (Verify(address))
             game_srv_object_addr = *(uintptr_t *)address;
@@ -50,12 +48,8 @@ namespace {
         GWCA_INFO("[SCAN] SendPacket = %p", SendPacket_Func);
         GWCA_INFO("[SCAN] CtoGSObjectPtr = %p", game_srv_object_addr);
 
-#ifdef _DEBUG
-#ifndef GWCA_CTOS_DISABLED
         GWCA_ASSERT(SendPacket_Func);
-#endif
         GWCA_ASSERT(game_srv_object_addr);
-#endif
         HookBase::CreateHook(SendPacket_Func, CtoSHandler_Func, (void**)&RetSendPacket);
 
     }
